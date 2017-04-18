ca53d7e841a15bd1e9f59c420e75d5e94647026d
RPetkovic <radicha@Macbookair-36.local>
2017-04-18 23:31:36 +0200

    ajout d'un 2e écran
    J’ai ajouté un écran après la création de l’événement en suivant la
    logique de Loris (si j’ai tout bien compris !??).

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -27,6 +27,12 @@ Template.main.events({
     header.style.cssText="visibility:hidden; position:absolute;";
     let formul = document.getElementById("form");
     formul.style.cssText="visibility:visible; position: absolute;";
+    // j'ai parametré la visibilité du 3e écran... dans la suite aussi
+    let evenement =document.getElementById("evenement");
+    evenement.style.cssText="visibility:hidden; position:absolute;";
+
+   
+
   }
 });
 
@@ -47,26 +53,40 @@ Template.formulaire.helpers({
 });
 
 //événement sur le deuxième header + ajouter un événement à la BD
-Template.formulaire.events({
-  'click button': function(){
+Template.formulaire.events({ // expliquer . events que fait-il ?
+  'click button': function(){ // as-tu une raison particulière d'avoir laissé cet espace dans la clé
     let header = document.getElementById("mainbody");
-    header.style.cssText="visibility:visible; position:absolute;";
+    header.style.cssText="visibility:hidden; position:absolute;";
     let formul = document.getElementById("form");
     formul.style.cssText="visibility:hidden; position: absolute;";
-  },
+    let evenement =document.getElementById("evenement");
+    evenement.style.cssText="visibility:visible; position:absolute;";
+
+
+  }, // expliquer cette construction ci-dessous.
   'submit .new-event'(event) {
     event.preventDefault();
 
     const target = event.target;
-    const name = target.text.value;
+    const name = target.text.value; // récupération du nom de l'événement
 
+    console.log(event);
+
+    // appel de l'objet BD pour insérer l'événement
     Events.insert({
       name,
     });
     target.text.value = '';
 
     let formul = document.getElementById("form");
-    formul.style.cssText="visibility:hidden; position: absolute;";
+    formul.style.cssText="visibility:hidden; position: absolute;"; 
+
+    // création d'une nouvelle variable pour retourner le contenu html "evenement"
+    // et ensuite on le rend visible
+
+    let evenement = document.getElementById('evenement');
+
+    evenement.style.visibility = 'visible';
   },
 });
 
@@ -74,4 +94,29 @@ Template.body.helpers({
   event() {
     return Events.find({});
   },
+
 });
+
+// nouveau template evenenment dans body html pour afficher le nouveau écran événement
+// je continue sur ta logique de copier-coller des events ()
+Template.evenement.events(
+{
+  'click button': function()
+  {
+    console.log("You clicked on the 1st button");
+
+    let header = document.getElementById("mainbody");
+
+    header.style.cssText="visibility:hidden; position:absolute;";
+
+    let formul = document.getElementById("form");
+
+    formul.style.cssText="visibility:visible; position: absolute;";
+
+    let evenement =document.getElementById("evenement");
+
+    evenement.style.cssText="visibility:visible; position:absolute;";
+
+  }
+}
+);
